66006ba401f759306f0e41db6ac1080708e0a672
JPlante9117 <jacrplante@gmail.com>
2019-12-12 09:47:08 -0500

    Done.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,15 +3,7 @@ class Appointment < ApplicationRecord
     belongs_to :patient
 
     def date
-        self.appointment_datetime.strftime("%B %e, %Y at %l:%M")
+        self.appointment_datetime.strftime("%B %e, %Y at %k:%M")
         #add %p if AM/PM is needed!
-    end
-
-    def doctor
-        self.doctor_id ? Doctor.find_or_create_by(id: doctor_id) : nil
-    end
-
-    def patient
-        self.patient_id ? Patient.find_or_create_by(id: patient_id) : nil
     end
 end
