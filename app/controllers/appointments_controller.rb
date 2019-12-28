fe5f131e3f21e976e9261d93174142577015e05e
github username <github email address>
2019-12-28 16:50:38 -0600

    Done.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,5 +4,18 @@ class AppointmentsController < ApplicationController
         @appointment = Appointment.find(params[:id])
     end 
 
+    def new
+        @appointment = Appointment.new
+    end 
+
+    def create 
+        @appointment = Appointment.create(appointment_params)
+    end 
+
+    private 
+
+    def appointment_params
+        params.require(:appointment).permit(:appointment_datetime)
+    end 
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191228195152) do
+ActiveRecord::Schema.define(version: 20191228221541) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20191228195152) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "age"
+    t.integer  "doctor_id"
   end
 
 end
